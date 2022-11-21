06b574458c8deb1494caf833167425d08b191bad
Mani03011998 <mani.ratna.monumamta@gmail.com>
2022-11-21 12:36:14 +0530

    Package.json updated

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const express = require ('express')
 const route = require('../route/route.js')
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 
-app.use(bodyParser)
+app.use(express.json())
 
 
-mongoose.connect("mongodb+srv://<chanda>:<QYho3EZNKLny4znA>@cluster0.gkrjc46.mongodb.net/group25Database",{
+mongoose.connect("mongodb+srv://chanda:QYho3EZNKLny4znA@cluster0.gkrjc46.mongodb.net/group25Database",{
    useNewUrlParser :true
 })
 .then(()=>console.log("mongoDb is connected"))
 .catch((err)=>console.log(err))
 
-app.listen(process.env.PORT || 3000 , function(){
-    console.log("Server is running on PORT" + " " + (proces.env.PORT || 3000))
+// app.use("/",route)
+app.listen(3000 , function(){
+    console.log("Server is running on PORT" + " " + 3000)
 })
-
-app.use("/",route)
