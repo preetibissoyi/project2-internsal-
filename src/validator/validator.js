fb2bb21b4320e4259f791ef395cb8fd6618438eb
jivan9822 <j.toshniwal@yahoo.co.in>
2022-11-23 00:36:14 +0800

    fetch done

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,48 +1,61 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
+const isValidShortName = function (name) {
+  if (/^[a-z]{2,10}/i.test(name)) {
+    return true;
+  }
+};
 
-      
+const isValidFullName = function (fullname) {
+  if (/^[a-zA-Z]+ [a-zA-Z]+$/i.test(fullname)) {
+    return true;
+  }
+};
+const isValidLink = function (link) {
+  if (
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.#?&//=]*)/i.test(
+      link
+    )
+  ) {
+    return true;
+  }
+};
 
-    const isValidShortName=function(name){
-        if(/^[a-z]{2,10}$/i.test(name)){
-            return true
-        }
-    }
+const isValidName = function (name) {
+  if (/^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/i.test(name)) {
+    return true;
+  }
+};
 
-    const isValidFullName=function(fullname){
-        if(/^[a-zA-Z]+ [a-zA-Z]+$/i.test(fullname)){
-            return true
-        }
-    }
-    const isValidLink=function(link){
-        if(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.#?&//=]*)/i.test(link)){
-            return true
-        }
-    }
-    
-    const isValidName=function(name){
-        if(/^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/i.test(name)){ 
-            return true
-        }
-    }
-    
-    const isValidId = function (data) {
-        return mongoose.Types.ObjectId.isValid(data); // intern
-      };
-    
-   
-    const isValidEmail = function (mail) {
-        if (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(mail)) {
-        return true;
-        }
-    }
-    const isValidMobile=function(mobile){
-        if(/^\d{0,3}[\- ]?[ ]*[\(]?\d{3}[\)]?[\- ]?[ ]*\d{3}[\- ]?[ ]*\d{4}$/.test(mobile)){
-            return true
-        }
-    }
-   
-  
-    
-    
-      module.exports={isValidEmail,isValidId,isValidMobile,isValidName,isValidShortName,isValidFullName,isValidLink,}
+const isValidId = function (data) {
+  return mongoose.Types.ObjectId.isValid(data); // intern
+};
+
+const isValidEmail = function (mail) {
+  if (
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      mail
+    )
+  ) {
+    return true;
+  }
+};
+const isValidMobile = function (mobile) {
+  if (
+    /^\d{0,3}[\- ]?[ ]*[\(]?\d{3}[\)]?[\- ]?[ ]*\d{3}[\- ]?[ ]*\d{4}$/.test(
+      mobile
+    )
+  ) {
+    return true;
+  }
+};
+
+module.exports = {
+  isValidEmail,
+  isValidId,
+  isValidMobile,
+  isValidName,
+  isValidShortName,
+  isValidFullName,
+  isValidLink,
+};
